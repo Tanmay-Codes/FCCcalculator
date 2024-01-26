0379ce6b3f7913179d6333d70d316eab03cb2238
Tanmay Agrawal <tanmayagcode@gmail.com>
2024-01-26 23:07:17 +0530

    add : calculation logic

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -20,16 +20,16 @@ export default function Calculator() {
     console.log(op)
     switch (op) {
       case 'add':
-        setInput(prev=> prev+num+"+")
+        setInput(prev=> prev.includes("=")? prev.split("=")[1]+"+" : prev+num+"+")
         break;
       case 'subtract':
-        setInput(prev=> prev+num+"-")
+        setInput(prev=> prev.includes("=")? prev.split("=")[1]+"-" : prev+num+"-")
         break;
       case 'multiply':
-        setInput(prev=> prev+num+"*")
+        setInput(prev=> prev.includes("=")? prev.split("=")[1]+"*" : prev+num+"*")
         break;
       case 'divide':
-        setInput(prev=> prev+num+"/")
+        setInput(prev=>prev.includes("=")? prev.split("=")[1]+"/" : prev+num+"/")
         break;
       case 'ac':
         setInput('')
@@ -37,9 +37,10 @@ export default function Calculator() {
         setNum(0)
         break;
       case 'equals':
-        setInput(prev=> prev+num)
-        console.log(input+num)
-        setNum(eval(input+num))
+        let result = eval(input+num)
+        setInput(prev=> prev+num+"="+result)
+        console.log(input+num+"="+result)
+        setNum(result)
         break;
     
       default:
@@ -49,8 +50,8 @@ export default function Calculator() {
   }
   return <div className="calculator">
   <div id="display">
-    <span className="display__old">{input}</span>
-    <span className="display__main">{num}</span>
+    <div className="display__old">{input}</div>
+    <div className="display__main">{num}</div>
   </div>
   <div className="keypad">
     <div className="left">
@@ -60,11 +61,11 @@ export default function Calculator() {
             
         </div>
         <div className="row2">
-            {keyPad.map(key => <button key={key.id} className="btn" onClick={()=> setNum(prev=> typeof prev !== 'string' && prev+key.num)} id={key.id}>
+            {keyPad.map(key => <button key={key.id} className="btn" onClick={()=> setNum(prev=> prev==0 ? key.num : prev.toString()+key.num)} id={key.id}>
                 {key.num}
             </button>)}
-            <button id="zero" className="btn__long" onClick={()=> setNum(prev=>prev+'0')} >0</button>
-            <button id="decimal" className="btn" onClick={()=> setNum(prev=>prev+".")} >.</button>
+            <button id="zero" className="btn__long" onClick={()=> setNum(prev=>prev=='0' ? '0' : prev+'0')} >0</button>
+            <button id="decimal" className="btn" onClick={()=> setNum(prev=>prev.toString().includes(".") ? prev : prev+".")} >.</button>
         </div>
     </div>
     <div className="right">
